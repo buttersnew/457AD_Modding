2d94317b4b95f5371462273fd8f212dfef27e9e1
EfeKaracar <efekaracar@gmail.com>
2023-01-28 07:36:40 -0500

    My compilers and scripts integration to _modules
    My compilers and scripts integration to _modules

---
+++ 457 AD Source Dev Version/efe_scripts.py
@@ -0,0 +1,23 @@
+# -*- coding: cp1254 -*-
+from header_common import *
+from header_operations import *
+from module_constants import *
+from module_constants import *
+from header_parties import *
+from header_skills import *
+from header_mission_templates import *
+from header_items import *
+from header_triggers import *
+from header_terrain_types import *
+from header_music import *
+from header_map_icons import *
+from ID_animations import *
+from module_factions import dplmc_factions_begin, dplmc_factions_end, dplmc_non_generic_factions_begin
+from ID_info_pages import *
+from header_presentations import tf_left_align
+from module_items import *
+
+efe_scripts = [
+("efe_init", []),
+]
+
